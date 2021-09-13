5b0103744333c5ea3624533ad6f9ee8bc974b64b
Keven Laptop <k.fazio@knights.ucf.edu>
2021-09-13 10:37:48 -0400

    ex18ch01 Initial commit

---
+++ exercise18/Challenge01/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+e
+5src/main/java/exercise18/challenge01/Challenge01.java,0\6\06607cdccf762462aad71989d383835d69c07605
---
+++ exercise18/Challenge01/src/main/java/exercise18/challenge01/Challenge01.java
@@ -0,0 +1,94 @@
+package exercise18.challenge01;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 first_name last_name
+ */
+
+import java.util.Scanner;
+
+public class Challenge01 {
+
+    /*
+     * Pseudocode:
+     * Prompt user "Press C to convert from Fahrenheit to Celsius."
+     * Prompt user "Press F to convert from Celsius to Fahrenheit."
+     * scan in answer
+     *
+     * if answer == C
+     *     letter = "C"
+     * else
+     *     letter = "F"
+     *
+     * Print "Your choice: 'letter'
+     *
+     * if answer == C (ignore case sensitivity)
+     *      Prompt user "Please enter the temperature in Fahrenheit:"
+     *      scan in answer
+     *      temp = ('answer' − 32) × 5 / 9 //Celsius
+     *      scale = Celsius
+     * else
+     *      Prompt user "Please enter the temperature in Celsius:"
+     *      scan in answer
+     *      temp = ('answer' × 9 / 5) + 32 //Fahrenheit
+     *      scale = Fahrenheit
+     *
+     * print "The temperature in 'scale' is 'temp'
+     *
+     */
+
+    public static void main(String[] args) {
+
+        System.out.printf("Press C to convert from Fahrenheit to Celsius.%n" +
+                "Press F to convert from Celsius to Fahrenheit.%n" +
+                "Your choice: ");
+
+        Scanner input = new Scanner(System.in);
+        String answer = input.nextLine();
+
+        answer = checkValid(answer);
+        printTemp(answer);
+    }
+
+    public static String checkValid (String answer){
+        while (!answer.equalsIgnoreCase("C") && !answer.equalsIgnoreCase("F")){
+            System.out.print("Invalid entry, please try again: ");
+            Scanner input = new Scanner(System.in);
+            answer = input.nextLine();
+        }
+        return answer;
+    }
+
+    public static void printTemp (String answer){
+        double finalTemp = 0;
+        String scale;
+
+        if (answer.equalsIgnoreCase("C")){
+            System.out.print("Please enter the temperature in Fahrenheit: ");
+            double temp = Double.parseDouble(isNumeric());
+            finalTemp = ((temp - 32) * .5556); //to Celsius
+            scale = "Celsius";
+        }
+        else{
+            System.out.print("Please enter the temperature in Celsius: ");
+            double temp = Double.parseDouble(isNumeric());
+            finalTemp = (temp * (1.8)) + 32; //to Fahrenheit
+            scale = "Fahrenheit";
+        }
+
+        System.out.printf("The temperature in %s is %.1f.%n", scale, finalTemp);
+    }
+
+    public static String isNumeric(){
+        Scanner input = new Scanner(System.in);
+        boolean numeric = input.hasNextDouble();
+        String answer = input.nextLine();
+
+        while (!numeric) {
+            System.out.print("Please answer with numeric values only: ");
+            numeric = input.hasNextDouble();
+            answer = input.nextLine();
+        }
+        return answer;
+    }
+}
