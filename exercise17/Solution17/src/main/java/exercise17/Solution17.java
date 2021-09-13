2e65b7dca3fd9b5701b805f7ccf25b9c1f97db4d
Keven Laptop <k.fazio@knights.ucf.edu>
2021-09-13 10:39:22 -0400

    deleted comments

--- exercise17/Solution17/src/main/java/exercise17/Solution17.java
+++ exercise17/Solution17/src/main/java/exercise17/Solution17.java
@@ -10,43 +10,6 @@ import java.util.Scanner;
 public class Solution17 {
 
     /*
-     * Create a program that prompts for your weight, gender, total alcohol consumed (in ounces),
-     * and the amount of time since your last drink. Calculate your blood alcohol content (BAC) using this formula
-     *
-     * BAC = (A × 5.14 / W × r) − .015 × H
-     * where:
-     * A is total alcohol consumed, in ounces (oz).
-     * W is body weight in pounds.
-     * r is the alcohol distribution ratio:
-     * 0.73 for men
-     * 0.66 for women
-     * H is number of hours since the last drink.
-     *
-     * Display whether or not it’s legal to drive by comparing the blood alcohol content to 0.08.
-     *
-     * Example Output:
-     * Enter a 1 if you are male or a 2 if you are female: 1
-     * How many ounces of alcohol did you have? 3
-     * What is your weight, in pounds? 175
-     * How many hours has it been since your last drink? 1
-     *
-     * Your BAC is 0.049323
-     * It is legal for you to drive.
-     *
-     * OR
-     *
-     * Enter a 1 if you are male or a 2 if you are female: 1
-     * How many ounces of alcohol did you have? 5
-     * What is your weight, in pounds? 175
-     * How many hours has it been since your last drink? 1
-     *
-     * Your BAC is 0.092206
-     * It is not legal for you to drive.
-     *
-     * Constraint:
-     * -Prevent the user from entering non-numeric values.
-     *
-     ---------------
      * Pseudocode:
      * Prompt user "Enter a 1 if you are male or a 2 if you are female:"
      * scan integer/check input is only numeric, if not, ask again.
