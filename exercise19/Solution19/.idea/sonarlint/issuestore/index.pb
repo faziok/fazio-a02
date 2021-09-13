ec58a2e0fc22d12b09f59407ff5423721503c943
Keven Laptop <k.fazio@knights.ucf.edu>
2021-09-13 11:37:28 -0400

    ex19 Inital commit

---
+++ exercise19/Solution19/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+X
+(src/main/java/exercise19/Solution19.java,8\3\83c5bcbaae5106f2b745d9154cbf9f4218046bcc
---
+++ exercise19/Solution19/src/main/java/exercise19/Solution19.java
@@ -0,0 +1,77 @@
+package exercise19;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 first_name last_name
+ */
+
+import java.util.Scanner;
+
+public class Solution19 {
+
+    /*
+     * Pseudocode
+     * Prompt user "Enter height (in inches):"
+     * scan in height/validate input is numeric
+     *
+     * Prompt user "Enter weight (in pounds):"
+     * scan in weight/validate input in numeric
+     *
+     * calculate BMI = (weight / (height × height)) * 703
+     * Print "Your BMI is 'bmi'."
+     *
+     * if bmi >= 18.5 && bmi <= 25
+     *      print "You are within the ideal weight range. You eat the perfect amount of french fries!"
+     * else if bmi < 18.5
+     *      print "You are underweight! Eat some french fries and go see your doctor."
+     * else
+     *      Print "You are overweight! Eat less french fries and go see your doctor."
+     *
+    */
+
+    public static void main(String[] args) {
+        System.out.print("Enter your height (in inches): ");
+        double height = Double.parseDouble(isNumeric());
+
+        System.out.print("Enter your weight (in pounds): ");
+        double weight = Double.parseDouble(isNumeric());
+
+        double bmi = getBMI(height, weight);
+
+        System.out.printf("Your BMI is %.1f%n%s%n", bmi, getRecommendation(bmi));
+
+
+    }
+
+    public static double getBMI (double height, double weight){
+        return (weight / (Math.pow(height, 2))) * 703;
+    }
+
+    public static String getRecommendation (double bmi){
+        String recommendation;
+
+        if (bmi >= 18.5 && bmi <= 25){
+            recommendation = "You are within the ideal weight range. You eat the perfect amount of french fries!";
+        }
+        else if (bmi < 18.5){
+            recommendation = "You are underweight! Eat some french fries and go see your doctor.";
+        }
+        else {
+            recommendation = "You are overweight! Eat less french fries and go see your doctor.";
+        }
+        return recommendation;
+    }
+
+    public static String isNumeric(){
+        Scanner input = new Scanner(System.in);
+        boolean numeric = input.hasNextDouble();
+        String answer = input.nextLine();
+
+        while (!numeric) {
+            System.out.print("Please answer with numeric values only: ");
+            numeric = input.hasNextDouble();
+            answer = input.nextLine();
+        }
+        return answer;
+    }
+}
