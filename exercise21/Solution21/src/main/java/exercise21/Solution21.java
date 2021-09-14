3931bba82aad3d18eec35b8f5e4030d836d21c8b
Keven Laptop <k.fazio@knights.ucf.edu>
2021-09-14 15:58:58 -0400

    add IF statement

--- exercise21/Solution21/src/main/java/exercise21/Solution21.java
+++ exercise21/Solution21/src/main/java/exercise21/Solution21.java
@@ -10,57 +10,59 @@ import java.util.Scanner;
 public class Solution21 {
 
     /*
-     * Write a program that converts a number from 1 to 12 to the corresponding month.
-     * Prompt for a number and display the corresponding calendar month, with 1 being January and 12 being December.
-     * For any value outside that range, display an appropriate error message.
-     *
-     * Example Output
-     * Please enter the number of the month: 3
-     * The name of the month is March.
-     *
-     * Constraints
-     * -Use a switch or case statement for this program.
-     * -Use a single output statement for this program.
-     *
      * Pseudocode:
      * Prompt user "Please enter the number of the month:"
      * scan in string month
      * (check if it is numeric, parse to int)
      *
-     * Switch monthString
-     *  case1 = January
-     *  case2 = February
-     *  .
-     *  .
-     *  .
-     *  case12 = December
-     *  default = "Error: Invalid month"
+     * declare string 'message'
      *
-     *  output "The name of the month is 'month'"
+     * Switch monthString
+     *  case1 = "January"
+     *  case2 = "February"
+     *  .
+     *  .
+     *  .
+     *  case12 = "December"
+     *  default = "Error: "
+     *
+     * if 1 > month >12
+     *      message = "Error: "
+     * else
+     *      message = "The name of the month is "
+     *
+     *  output "'message' 'month'."
      */
+
     public static void main(String[] args) {
         System.out.print("Please enter the number of the month: ");
         int month = Integer.parseInt(isNumeric());
-        int output = 1;
         String message;
 
         String monthString = switch (month) {
-            case 1 -> "The name of the month is January";
-            case 2 -> "The name of the month is February";
-            case 3 -> "The name of the month is March";
-            case 4 -> "The name of the month is April";
-            case 5 -> "The name of the month is May";
-            case 6 -> "The name of the month is June";
-            case 7 -> "The name of the month is July";
-            case 8 -> "The name of the month is August";
-            case 9 -> "The name of the month is September";
-            case 10 -> "The name of the month is October";
-            case 11 -> "The name of the month is November";
-            case 12 -> "The name of the month is December";
-            default -> "Error: Invalid month";
+            case 1 -> "January";
+            case 2 -> "February";
+            case 3 -> "March";
+            case 4 -> "April";
+            case 5 -> "May";
+            case 6 -> "June";
+            case 7 -> "July";
+            case 8 -> "August";
+            case 9 -> "September";
+            case 10 -> "October";
+            case 11 -> "November";
+            case 12 -> "December";
+            default -> "Invalid month";
         };
 
-            System.out.println(monthString);
+        if (month < 0 || month > 12){
+            message = "Error: ";
+        }
+        else{
+            message = "The name of the month is ";
+        }
+
+            System.out.printf("%s%s.%n", message, monthString);
     }
 
     public static String isNumeric(){
@@ -74,15 +76,4 @@ public class Solution21 {
         }
         return number;
     }
-
-    /*
-    //Check if input is in range
-    public static int inRange(int number){
-        while (number < 1 || number > 12){
-            System.out.print("Please enter a number 1-12 for the month: ");
-            number = Integer.parseInt(isNumeric());
-        }
-        return number;
-    }
-     */
 }
